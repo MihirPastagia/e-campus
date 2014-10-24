f0cf83f8b58d727593f66e395f74908f4d875ed3
MihirPastagia <pastagiamihir@gmail.com>
2014-10-24 15:53:45 +0530

    Connection1

--- src/Connction.java
+++ src/Connction.java
@@ -1,9 +1,16 @@
 import java.sql.*;
+
 import javax.swing.*;
+
+import java.net.*;
+import java.io.*;
+
 public class Connction {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		int port = Integer.parseInt(args[0]);
+		int backlog=Integer.parseInt(args[1]);
 		 Connection c = null;
 		    try {
 		      Class.forName("org.sqlite.JDBC");
@@ -13,6 +20,10 @@ public class Connction {
 		      System.exit(0);
 		    }
 		    System.out.println("Opened database successfully");
+		    Thread u=new server(port,backlog);
+		    u.start();
 	}
 
 }
+
+
